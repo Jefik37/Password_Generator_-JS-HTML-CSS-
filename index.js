3402de5ad75558ac00b91f625493143a56821f2e
Erick <erick1204200100@gmail.com>
2025-12-16 21:57:05 -0300

    Clip button added

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const MIN_CHAR = 8;
+const MAX_CHAR = 256
+
 //Constant variables that will deal with the first option
 const NUMBER_OF_CHARS = document.getElementById("inumber");
 const INCLUDE_SYMBOLS = document.getElementById("ichar_opt");
@@ -16,9 +19,9 @@ const RESPONSE_OPT1 = document.getElementById("passwordPlacement1");
 const RESPONSE_OPT2 = document.getElementById("passwordPlacement2");
 
 HTML_CODE = function(variable) {
-    return `<div style = " margin: auto;width: 80%; background-color: white; margin-top: 20px; padding: 20px;
+    return `<div style = "margin: auto;width: 80%; background-color: white; margin-top: 20px; padding: 20px;
         border-color: black; border-style: solid; border-width: 4px; border-radius: 10px; margin-bottom: 20px;">
-        <p style = 'margin: 0px; padding: 0px; font-family: "cascadia code semibold", monospace; font-size: 1.4em;'>${variable}</p></div>`
+        <p style = 'text-align: left; margin: 0px; padding: 0px; font-family: "cascadia code semibold", monospace; font-size: 1.4em;'>${variable}</p></div>`
 }
 
 function selectRandom(inputString) {
@@ -85,6 +88,10 @@ function genPassword(size, required_char, symbols_signal = true, upper_signal = 
     return chaoticPassword
 }
 
+let REAL_SEC_PASSWORD;
+let REAL_FIRST_PASSWORD;
+const ALERT_MSG = "Generate the password or wait it to be processed if you've already created it.";
+
 SUBMIT_OPT1.onclick = function() {
     //Getting the input values from the HTML widgets.
     const PASSWORD_SIZE = Number(NUMBER_OF_CHARS.value);
@@ -96,12 +103,12 @@ SUBMIT_OPT1.onclick = function() {
 
     //Error mesages to be shown on screen
     const ERR_MSG1 = "You must fill at least one box";
-    const ERR_MSG2 = "The number of elements in the password must be between 8 and 50."
+    const ERR_MSG2 = `The number of elements in the password must be between ${MIN_CHAR} and ${MAX_CHAR}.`
 
     //Operating with respect to conditions
     if (!PERMISSION_SYMBOLS && !PERMISSION_UPPER && !PERMISSION_NUMBERS && !PERMISSION_LOWER) {
         RESPONSE_OPT1.innerHTML = `<p style = "color: red; padding: 0px; text-align: center;"> ${ERR_MSG1} </p>`;
-    } else if (PASSWORD_SIZE < 8 || PASSWORD_SIZE > 50) {
+    } else if (PASSWORD_SIZE < MIN_CHAR || PASSWORD_SIZE > MAX_CHAR) {
         RESPONSE_OPT1.innerHTML = `<p style = "color: red; padding: 0px; text-align: center;"> ${ERR_MSG2} </p>`
     } else {
         for (let i = 0; i < 50; i++) {
@@ -111,17 +118,17 @@ SUBMIT_OPT1.onclick = function() {
                 RESPONSE_OPT1.innerHTML = RESPONSE1_HTML;}, i**2);
         }
         setTimeout(function() {
-            let password = genPassword(PASSWORD_SIZE, MUST_BE, PERMISSION_SYMBOLS, PERMISSION_UPPER, PERMISSION_LOWER, PERMISSION_NUMBERS);
-            let RESPONSE1_HTML = HTML_CODE("&#x1F513; Your password >> " + password);
+            REAL_FIRST_PASSWORD = genPassword(PASSWORD_SIZE, MUST_BE, PERMISSION_SYMBOLS, PERMISSION_UPPER, PERMISSION_LOWER, PERMISSION_NUMBERS);
+            let RESPONSE1_HTML = HTML_CODE("&#x1F513; Your password >> " + REAL_FIRST_PASSWORD);
             RESPONSE_OPT1.innerHTML = RESPONSE1_HTML;}, 50**2 + 1);
     }
 }
 
 SUBMIT_OPT2.onclick = function() {
     const INPUT = INPUT_TEXT.value;
-    const ERR_MSG2 = "The number of elements inside the text must be between 8 and 50.";
+    const ERR_MSG2 = `The number of elements inside the text must be between ${MIN_CHAR} and ${MAX_CHAR}.`;
     const size = INPUT.length;
-    if (size < 8 || size > 50) {
+    if (size < MIN_CHAR || size > MAX_CHAR) {
         RESPONSE_OPT2.innerHTML = "<p style = 'color: red; padding: 0px; text-align: center'>" + ERR_MSG2 + "</p>";
     }
     else {
@@ -132,8 +139,24 @@ SUBMIT_OPT2.onclick = function() {
                 RESPONSE_OPT2.innerHTML = RESPONSE2_HTML; console.log(i)}, i**2);
         }
         setTimeout(function() {
-            let MESSED_PASSWORD = shuffleString(INPUT);
-            let RESPONSE2_HTML = HTML_CODE("&#x1F513; Your password >> " + MESSED_PASSWORD);
+            REAL_SEC_PASSWORD = shuffleString(INPUT);
+            let RESPONSE2_HTML = HTML_CODE("&#x1F513; Your password >> " + REAL_SEC_PASSWORD);
             RESPONSE_OPT2.innerHTML = RESPONSE2_HTML;}, 50**2 + 1);
+    }
+}
+
+document.getElementById("clip1").onclick = function() {
+    if (REAL_FIRST_PASSWORD !== undefined) {
+        navigator.clipboard.writeText(REAL_FIRST_PASSWORD);
+    } else {
+        alert(ALERT_MSG);
+    }
+}
+
+document.getElementById("clip2").onclick = function() {
+    if (REAL_SEC_PASSWORD !== undefined) {
+        navigator.clipboard.writeText(REAL_SEC_PASSWORD);
+    } else {
+        alert(ALERT_MSG);
     }
 }
